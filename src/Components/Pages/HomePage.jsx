7e327ce0a933d63d89332f0ac14e5d3d7ed9f2bc
Andrea Lauro <125922974+Andreala94@users.noreply.github.com>
2023-09-26 20:34:52 +0200

    commit finish

--- src/Components/Pages/HomePage.jsx
+++ src/Components/Pages/HomePage.jsx
@@ -42,7 +42,7 @@ function HomePage(){
     <NavBar totaleEventi={totaleEventi} setArrayEventi={setArrayEventi} />
     <Carosello />  
     <NewEvents  />
-    {isLoading ? <span className="ms-2" style={{color: "white"}}>Loading...</span> : <ListaEventi eventi={arrayEventi} />}
+    {isLoading ? <span className="ms-2 fw-bold" style={{color: "white"}}>Loading...</span> : <ListaEventi eventi={arrayEventi} />}
     <Footer />
    
     </>
